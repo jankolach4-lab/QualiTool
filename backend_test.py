070030e3eb8eae065629936a703f6a488b90488d
Dashboard Admin <admin@dashboard.de>
2025-10-21 19:00:53 +0000

    auto-commit for d2bcf7b9-05c9-4b16-9b70-a026636b27cb

--- backend_test.py
+++ backend_test.py
@@ -3,7 +3,7 @@ import sys
 from datetime import datetime
 
 class SimpleAPITester:
-    def __init__(self, base_url="https://projectanalytics.preview.emergentagent.com"):
+    def __init__(self, base_url="https://address-manager-6.preview.emergentagent.com"):
         self.base_url = base_url
         self.tests_run = 0
         self.tests_passed = 0
